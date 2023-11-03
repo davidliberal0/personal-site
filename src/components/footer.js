46e9b3995d0a1cc30e3c800bed7adc640f05b181
davidliberal0 <davidliberal0@gmail.com>
2023-11-03 13:07:42 -0400

    add credits to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://www.linkedin.com/in/davidliberal/">
-          <div>David Liberal</div>
+          <div>Designed by Brittany Chiang</div>
         </a>
       </StyledCredit>
     </StyledFooter>
