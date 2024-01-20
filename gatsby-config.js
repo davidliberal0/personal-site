66d6642e343f141ccaa563a9bd87809787e373e9
David Liberal <davidliberal0@gmail.com>
2024-01-20 16:27:17 -0500

    add site meta for search engine optimization

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,10 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'David Liberal',
-    description:
-      'David Liberal is a software engineer who specializes in building and designing) exceptional products.',
+    description: 'David Liberal Software Engineer',
     siteUrl: 'https://davidliberal.com', // No trailing slash allowed!
     image: '', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'David Liberal',
+        short_name: 'David Liberal',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
